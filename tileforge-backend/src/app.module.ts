df415056235fe7a466e7117ce1c61be498b9eab5
Felipe Marinho de Freitas <f.marinho162001@gmail.com>
2026-08-10 10:54:33 -0300

    reset project

--- tileforge-backend/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './infra/prisma/prisma.module';
-
-@Module({
-  imports: [PrismaModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
